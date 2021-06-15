e0759c0d48a35e244907ab506690db23925995c9
Faizaan Gagan <fzngagan@gmail.com>
2021-06-15 13:12:05 +0530

    added missing jobs folder

---
+++ jobs/sf_update_group_membership.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Jobs
+  class SfUpdateGroupMembership < ::Jobs::Base
+    def execute(args)
+      user = User.find(args[:user_id])
+      group = Group.find(args[:group_id])
+      manager = DiscourseSalesforce::GroupMembershipManager.new(user, group)
+
+      if args[:action] == "add"
+        manager.add_user_to_group
+      else
+        manager.remove_user_from_group
+      end
+    end
+  end
+end
