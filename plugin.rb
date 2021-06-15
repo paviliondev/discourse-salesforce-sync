27d0f85722736dcaa451de9c684a55ed8570e3ea
Faizaan Gagan <fzngagan@gmail.com>
2021-06-15 13:00:20 +0530

    perform Contact record updates using sidekiq queues

--- plugin.rb
+++ plugin.rb
@@ -16,20 +16,25 @@ after_initialize do
     "../lib/sf_rest_client.rb",
     "../lib/sf_contact_updater.rb",
     "../lib/sf_group_membership_manager.rb",
-    "../jobs/update_group_membership.rb",
+    "../jobs/sf_update_contact_record.rb",
+    "../jobs/sf_update_group_membership.rb"
   ].each do |path|
     load File.expand_path(path, __FILE__)
   end
 end
 
 on(:user_created) do |user|
-  updater = DiscourseSalesforce::ContactUpdater.new(user)
-  updater.create_or_update_record
+  ::Jobs.enqueue(
+    :sf_update_contact_record,
+    user_id: user.id,
+  )
 end
 
 on(:user_updated) do |user|
-  updater = DiscourseSalesforce::ContactUpdater.new(user)
-  updater.create_or_update_record
+  ::Jobs.enqueue(
+    :sf_update_contact_record,
+    user_id: user.id,
+  )
 end
 
 on(:site_setting_changed) do |name, _, _|
@@ -49,7 +54,7 @@ end
 on(:user_added_to_group) do |user, group|
   client = ::DiscourseSalesforce::RestClient.instance
   ::Jobs.enqueue(
-    :update_group_membership,
+    :sf_update_group_membership,
     user_id: user.id,
     group_id: group.id,
     action: "add"
@@ -58,7 +63,7 @@ end
 
 on(:user_removed_from_group) do |user, group|
   ::Jobs.enqueue(
-    :update_group_membership,
+    :sf_update_group_membership,
     user_id: user.id,
     group_id: group.id,
     action: "remove"
